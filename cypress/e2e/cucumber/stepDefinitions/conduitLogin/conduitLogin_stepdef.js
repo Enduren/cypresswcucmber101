52b0402c80350c93e89164469877e597d6827b37
Dexter Dwayne Tennison <83023757+Enduren@users.noreply.github.com>
2024-05-27 09:28:03 -0500

    added files
    added folders from local host

---
+++ cypress/e2e/cucumber/stepDefinitions/conduitLogin/conduitLogin_stepdef.js
@@ -0,0 +1,32 @@
+import{Given,When,Then} from 'cypress-cucumber-preprocessor/steps'
+
+
+
+Given('User is on the login page',function () {
+    cy.visit('https://react-redux.realworld.io/')
+    cy.get('a[href="#login"]').click()
+    
+})
+
+
+When('User login with valid credentials',function () {
+    cy.get('input[placeholder="Email"]').type('enter email')
+    cy.get('input[placeholder="Password"]').type('enterpassword')
+    cy.get('button[type="submit"]').click()
+    
+})
+
+When('User click on the settings button',function () {
+    cy.get('a[href="#settings"]').click()
+    
+})
+
+When('User click on the logout button',function () {
+    cy.get('.btn.btn-outline-danger').click()
+    
+})
+
+Then('User should be routed back to login page',function () {
+    cy.title().should('eq', 'Conduit')
+
+})
